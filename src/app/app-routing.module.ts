3219fe9205bd742da246b4fba13f279632396361
souhayl-naimi <souhayl630@gmail.com>
2021-01-15 13:30:29 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProductComponent} from './product/product.component';
+import {CustomerComponent} from './customer/customer.component';
+import {BillComponent} from './bill/bill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "products",
+    component: ProductComponent
+  },
+  {
+    path: "customers",
+    component: CustomerComponent
+  },
+  {
+    path: "fullBill",
+    component: BillComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CustomerComponent } from './customer/customer.component';
+import {HttpClientModule} from '@angular/common/http';
+import { BillComponent } from './bill/bill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CustomerComponent,
+    BillComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
